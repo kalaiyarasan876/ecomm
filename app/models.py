f35b3dcc3cd053a998627d091b5cd3614ecedbaa
kalaiyarasan876 <160699419+kalaiyarasan876@users.noreply.github.com>
2024-07-03 19:13:51 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,97 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+STATE_CHOICES =(
+  ("Andhra Pradesh","Andhra Pradesh"),
+  ("Arunachal Pradesh ","Arunachal Pradesh "),
+  ("Assam","Assam"),
+  ("Bihar","Bihar"),
+  ("Chhattisgarh","Chhattisgarh"),
+  ("Goa","Goa"),
+  ("Gujarat","Gujarat"),
+  ("Haryana","Haryana"),
+  ("Himachal Pradesh","Himachal Pradesh"),
+  ("Jammu and Kashmir ","Jammu and Kashmir "),
+  ("Jharkhand","Jharkhand"),
+  ("Karnataka","Karnataka"),
+  ("Kerala","Kerala"),
+  ("Madhya Pradesh","Madhya Pradesh"),
+  ("Maharashtra","Maharashtra"),
+  ("Manipur","Manipur"),
+  ("Meghalaya","Meghalaya"),
+  ("Mizoram","Mizoram"),
+  ("Nagaland","Nagaland"),
+  ("Odisha","Odisha"),
+  ("Punjab","Punjab"),
+  ("Rajasthan","Rajasthan"),
+  ("Sikkim","Sikkim"),
+  ("Tamil Nadu","Tamil Nadu"),
+  ("Telangana","Telangana"),
+  ("Tripura","Tripura"),
+  ("Uttar Pradesh","Uttar Pradesh"),
+  ("Uttarakhand","Uttarakhand"),
+  ("West Bengal","West Bengal"),
+  ("Andaman and Nicobar Islands","Andaman and Nicobar Islands"),
+  ("Chandigarh","Chandigarh"),
+  ("Dadra and Nagar Haveli","Dadra and Nagar Haveli"),
+  ("Daman and Diu","Daman and Diu"),
+  ("Lakshadweep","Lakshadweep"),
+  ("National Capital Territory of Delhi","National Capital Territory of Delhi"),
+  ("Puducherry","Puducherry")
+  )
+
+CATEGORY_CHOICES = (
+    ('CR', 'Crud'),
+    ('ML', 'Milk'),
+    ('LS', 'Lassi'),
+    ('MS', 'Milkshake'),
+    ('PN', 'Paneer'),
+    ('GH', 'Ghee'),
+    ('CZ', 'Cheese'),
+    ('IC', 'Ice-Creams'),
+)
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    composition = models.TextField(default='')
+    prodapp = models.TextField(default='')
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    product_image = models.ImageField(upload_to='product')
+    
+    
+    def __str__(self):
+        return self.title
+    
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    mobile = models.IntegerField(default=0)
+    zipcode = models.IntegerField()  # Set a default value here
+    state = models.CharField(choices=STATE_CHOICES, max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.name 
+    
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveBigIntegerField(default=1)
+    
+    @property
+    def total_cost(self):
+        return self.quantity * self.product.discounted_price
+   
+    
+    
+    
+    
+    
+    
